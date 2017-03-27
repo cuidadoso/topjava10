75e49bc5d9d0d5225da098f860dc08daff65c2de
cuidadoso <alexander_s@front.ru>
2017-03-27 22:05:02 +0200

    HW01 Add in ui storybook, addons for storybook, gh-pages.

---
+++ ui/src/component/Avatar/Avatar.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Alejandro on 26.03.2017.
+ */
+import React, { Component, PropTypes as pt } from 'react';
+import ReactImageFallback from "react-image-fallback";
+import './Avatar.css';
+
+class Avatar extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.children}
+                <ReactImageFallback
+                    src={this.props.src}
+                    fallbackImage={this.props.fallback}
+                    initialImage={this.props.initial}
+                    alt={this.props.alt}
+                    className={this.props.className} />
+            </div>
+        );
+    }
+}
+
+Avatar.displayName = 'Avatar';
+Avatar.propTypes = {
+    children:   pt.string,
+    src:        pt.string.isRequired,
+    fallback:   pt.string.isRequired,
+    initial:    pt.string.isRequired,
+    alt:        pt.string,
+    className:  pt.string,
+};
+Avatar.defaultProps = {
+    className: 'avatar-image',
+};
+
+export default Avatar;
