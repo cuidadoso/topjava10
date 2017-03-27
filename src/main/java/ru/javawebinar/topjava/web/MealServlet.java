6c5e5fe6f1461bedcaea178a739a969aace9208a
cuidadoso <alexander_s@front.ru>
2017-03-27 18:42:44 +0200

    HW01 1

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by apyreev on 27-Mar-17.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to meals");
+
+        request.setAttribute("meals", MealsUtil.getWithExceed(MealsUtil.MEALS, 2000));
+        request.setAttribute("a", 2000);
+
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
+    }
+}
