f8ddc672fc2259c1b7d3a823e14b82af5289bc88
cuidadoso <alexander_s@front.ru>
2017-04-03 18:35:17 +0200

    HW02 Refactoring all layers for meal.

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -4,7 +4,7 @@ import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.javawebinar.topjava.AuthorizedUser;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -19,19 +19,19 @@ import java.util.stream.Collectors;
  * GKislin
  * 15.09.2015.
  */
+@Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
     private static final Logger LOG = LoggerFactory.getLogger(InMemoryMealRepositoryImpl.class);
     //private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private Table<Integer, Integer, Meal> repository = HashBasedTable.create();
     private AtomicInteger counter = new AtomicInteger(0);
-    private int userId = AuthorizedUser.id();
 
     {
-        MealsUtil.MEALS.forEach(this::save);
+        MealsUtil.MEALS.forEach((meal) -> save(meal, 1));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
         LOG.info("save " + meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
@@ -41,20 +41,20 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public boolean delete(int id) {
+    public boolean delete(int id, int userId) {
         LOG.info("delete " + id);
         repository.remove(id, userId);
         return Objects.isNull(repository.get(id, userId));
     }
 
     @Override
-    public Meal get(int id) {
+    public Meal get(int id, int userId) {
         LOG.info("get " + id);
         return repository.get(id, userId);
     }
 
     @Override
-    public Collection<Meal> getAll() {
+    public Collection<Meal> getAll(int userId) {
         LOG.info("getAll");
         return repository.column(userId).values().stream()
                 .sorted(Comparator.comparing(Meal::getDateTime).reversed())
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,35 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
+import java.util.Collection;
+
+@Controller
 public class MealRestController {
+
+    private int userId = AuthorizedUser.id();
+
+    @Autowired
     private MealService service;
 
+    public Collection<Meal> getAll() {
+        return service.getAll(userId);
+    }
+
+    public Meal get(int id) {
+        return service.get(id, userId);
+    }
+
+    public Meal create(Meal meal) {
+        return service.save(meal, userId);
+    }
+
+
+    public void delete(int id) {
+        service.delete(id, userId);
+    }
 }
